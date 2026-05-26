9d26e64da2ebd21409dc3e6050875db10efc8209
saithis <1547453+saithis@users.noreply.github.com>
2026-05-26 21:08:42 +0200

    Fix MA0036: Make PlaygroundHostAppMarker static

--- examples/PlaygroundHost/PlaygroundHostAppMarker.cs
+++ examples/PlaygroundHost/PlaygroundHostAppMarker.cs
@@ -4,4 +4,4 @@ namespace PlaygroundHost;
 /// Assembly entry marker for MVC testing WebApplicationFactory so tests can target this app
 /// without colliding with other Program types in the same test assembly.
 /// </summary>
-public sealed class PlaygroundHostAppMarker;
+public static class PlaygroundHostAppMarker;
